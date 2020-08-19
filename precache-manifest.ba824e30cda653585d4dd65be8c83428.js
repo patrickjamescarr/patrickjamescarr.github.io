59900cc014ed9070bce6d1d31d0a2a42dd4cf52c
Patrick Carr <carr.patrickjames@gmail.com>
2020-08-19 23:23:03 +0100

    Updates

---
+++ precache-manifest.ba824e30cda653585d4dd65be8c83428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3fad08188fbc511658afbb6a216e00",
+    "url": "/img-2-brix/index.html"
+  },
+  {
+    "revision": "cb865a2be4ef0705c3e0",
+    "url": "/img-2-brix/static/css/main.d69bda90.chunk.css"
+  },
+  {
+    "revision": "87e988b17bf1517effeb",
+    "url": "/img-2-brix/static/js/2.6921307f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/img-2-brix/static/js/2.6921307f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb865a2be4ef0705c3e0",
+    "url": "/img-2-brix/static/js/main.ded5a978.chunk.js"
+  },
+  {
+    "revision": "a3cae97fb630beba29fc",
+    "url": "/img-2-brix/static/js/runtime-main.7bd96408.js"
+  }
+]);
